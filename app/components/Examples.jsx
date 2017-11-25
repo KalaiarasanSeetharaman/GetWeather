b71f4b26a0ffe406e3bbe6001ab5664cf9921536
kalaiarasan.s <kalaiarasan.s@SHLOKINDIA.COM>
2017-11-25 14:50:45 +0530

    Changes Made

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link}  = require('react-router');
 
 var Examples = React.createClass({
 		render:function(){
 			return (
-				<h3> Examples Component </h3>
+				<div>
+					<h1 className="text-center" id="examples"> Examples Component </h1>
+					<p>Some Pages</p>
+					<ol>
+						<li>
+							<Link to="/?location=Karur">Karur</Link>
+						</li>
+						<li>
+							<Link to="/?location=Erode">Erode</Link>
+						</li>
+					</ol>
+				</div>
 			);
 		}
 
