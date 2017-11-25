f05d59ad8b6c9611b6009a7941c15f8be3ce8c65
kalaiarasan.s <kalaiarasan.s@SHLOKINDIA.COM>
2017-11-25 14:33:50 +0530

    Changes Made

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,13 +2,39 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
+		onSearch:function(e){
+			e.preventDefault();
+			alert("HI");
+		},
 		render:function(){
 			return (
-				<div>
-					<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-					<Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}} >About</Link>
-					<Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
-					<a href="#/about"> Go To About </a>
+				<div className="top-bar">
+					<div className="top-bar-left">
+						<ul className="menu">
+							<li className="menu-text">Recat Weather App</li>
+							<li>
+								<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+							</li>
+							<li>
+								<Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}} >About</Link>
+							</li>
+							<li>
+								<Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="top-bar-right">
+						<form onSubmit={this.onSearch}>
+							<ul className="menu">
+							      <li>
+							      	<input type="search" placeholder="Search"/>
+							      </li>
+							      <li>
+							      	<input type="submit" className="button" value="Get Weather"/>
+							      </li>
+							  </ul>
+						</form>
+					</div>
 				</div>
 			);
 		}
@@ -16,3 +42,10 @@ var Nav = React.createClass({
 });
 
 module.exports = Nav;
+
+/*<div>
+					<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+					<Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}} >About</Link>
+					<Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+					<a href="#/about"> Go To About </a>
+				</div>*/
